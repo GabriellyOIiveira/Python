0697802695a7fe1cdcceeb652c964535e8bdda27
Gabrielly Oliveira <gabriellysilva1669@gmail.com>
2024-08-28 17:26:45 -0300

    Add files via upload

---
+++ 01-aula01.py
@@ -0,0 +1,19 @@
+# Aqui eu posso adicionar comentarios ao meu codigo
+# Continuar na linha de baixo
+print("Gaby") # Comentario do codigo
+print('Gaby')
+print('Gaby "byby" Oliveira')
+print("Gaby 'byby' Oliveira")
+
+teste = "Oi"
+print(teste)
+print(type(teste))
+teste = 17
+print(teste)
+print(type(teste))
+teste = 12.1
+print(teste)
+print(type(teste))
+teste = False
+print(teste)
+print(type(teste))
