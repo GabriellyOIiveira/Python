4a85a56a2aaf92852079675cf9d9a4f6c9f4a0b6
Gabrielly Oliveira <gabriellysilva1669@gmail.com>
2024-09-11 17:32:09 -0300

    Add files via upload

---
+++ Operação-aritmetrica.py
@@ -0,0 +1,32 @@
+
+# Operadores aritméticos
+valorA = float(input("Digite o valor de A: "))
+valorB = float(input("Digite o valor de B: "))
+
+# Soma
+soma = valorA + valorB
+print(f"A soma = {soma}")
+
+# Subtração
+Subtracao = valorA - valorB
+print(f"A subtração = {Subtracao}")
+
+# Multiplicação
+multiplicacao = valorA * valorB
+print(f"A mutiplicação = {multiplicacao}")
+
+# Divisão
+Divisao = valorA / valorB
+print(f"A Divisão = {Divisao}")
+
+# Divisão inteira
+Divisao_inteiro = valorA // valorB
+print(f"A divisão inteira = {Divisao_inteiro}")
+
+# Resto da Divisão
+modulo = valorA % valorB
+print(f"O resto da divisão = {modulo}")
+
+# Exponenciação
+exponenciacao = valorA ** valorB
+print(f"A exponenciação = {exponenciacao}")
