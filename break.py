653384b19bfe82504bd54d3b88e8d42fbc8e326d
Gabrielly Oliveira <gabriellysilva1669@gmail.com>
2024-10-09 17:27:34 -0300

    Add files via upload

---
+++ break.py
@@ -0,0 +1,9 @@
+while True:
+    nome = input('Digite o seu nome ou escreva "sair": ')
+
+    if nome == 'sair':
+        break
+
+    print(f'Seu nome é {nome}')
+
+print('acabou :D')
---
+++ continue.py
@@ -0,0 +1,12 @@
+contador = 0
+
+while contador <= 40:
+    contador += 1
+
+    if (contador % 4 == 0):
+        print("pim")
+        continue
+
+    print(contador)
+
+print ("Fim do programa")
